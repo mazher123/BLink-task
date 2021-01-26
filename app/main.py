f7cc5a7dddc7ed1c330ac1f99b1ff8b48b178970
mazher123 <mazherulislam6@gmail.com>
2021-01-26 22:02:51 +0600

    flask installed and configured and request body initializzed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import flask
 from flask import Flask , jsonify,request
 
 
@@ -21,5 +22,13 @@ app = Flask(__name__)
 @app.route('/',defaults={ 'BanglaLink : Start Something new '})
 
 @app.route('/time-difference',methods=['GET'])
+def CalculateTimeInSec():
+        Requests =request.get_data()
+        # accept two test case input of Task A in body 
+        #t1 = Test 1 input 1
+        #t2 = Test 1 input 2
+        #s1 = Test 2 input 1
+        #s2 = Test 2 input 2
 
-        
+        data=json.loads(Requests)
+  
