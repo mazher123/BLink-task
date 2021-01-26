c19a44c9a065cb7917205f5e3c9fc890dcde9bb8
mazher123 <mazherulislam6@gmail.com>
2021-01-27 03:10:36 +0600

    node Id Created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import json
 from datetime import datetime
 import flask
+
+import os
+
 from flask import Flask , jsonify,request
 
 
@@ -19,31 +22,37 @@ def TimeDifferenceInSec(t1, t2):
 
 
 app = Flask(__name__)
-@app.route('/',defaults={ 'BanglaLink : Start Something new '})
 
-@app.route('/time-difference',methods=['GET'])
+ 
+input1 ='Sun 10 May 2015 13:54:36 -0700'
+input2 ='Sun 10 May 2015 13:54:36 -0000'
+input3 ='Sat 02 May 2015 19:54:36 +0530'
+input4 ='Fri 01 May 2015 13:54:36 -0000'
+
+
+@app.route('/',methods=['GET'])
 def CalculateTimeInSec():
-        Requests =request.get_data()
+        
         # accept two test case input of Task A in body 
         #t1 = Test 1 input 1
         #t2 = Test 1 input 2
         #s1 = Test 2 input 1
         #s2 = Test 2 input 2
 
-        data=json.loads(Requests)
-
-        t1 = str(data['t1'])
-        t2 = str(data['t2'])
-        s1 =str(data['s1'])
-        s2 = str(data['s2'])
+        t1 = str(input1)
+        t2 = str(input2)
+        s1 =str(input3)
+        s2 = str(input4)
         testCase1 = TimeDifferenceInSec(t1,t2)
         testCase2 = TimeDifferenceInSec(s1,s2)
+        host= os.getenv('HOSTNAME')
+        ResData= json.dumps({'id': host,"result":[testCase1, testCase2]})
 
-        ResData= json.dumps([testCase1, testCase2])
+  
         resp =  flask.Response(ResData)    
         resp.headers['Access-Control-Allow-Origin'] = '*'
         resp.content_type = 'application/json'
         return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=False)   
+    app.run(host="0.0.0.0",debug=True,port=8000)  
