42fc2e317f72ff0dc7a2e77bdcc80ff8d829548c
mazher123 <mazherulislam6@gmail.com>
2021-01-26 20:56:08 +0600

    Task A is completed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
-import math
-import os
-import random
-import re
-import sys
 from datetime import datetime
 
 #This funtion Calculate time difference in seconds 
@@ -15,5 +10,18 @@ def TimeDifferenceInSec(t1, t2):
     t1 = datetime.strptime(t1, time_format)
     t2 = datetime.strptime(t2, time_format)
 
-    #return  time difference in seconds 
-    return str(int(abs((t1-t2).total_seconds())))  
+    #return  time difference in seconds
+    return str(int(abs((t1-t2).total_seconds()))) 
+
+
+#taking input
+t = int(input())
+
+#iterate through t and take input 
+for t_itr in range(t):
+    t1 = input()
+    t2= input()
+
+    #final result is fetch from the function 
+    result = TimeDifferenceInSec(t1,t2)
+    print(result)
